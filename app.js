9cc2636a0214daa605e2ae5d1558cfecdbfdd5ba
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-03-21 12:50:19 -0700

    filled in async functions for getDogs and getDogById

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import { renderDogCard } from './render-utils.js';
 
 const dogListContainer = document.getElementById('dog-list-container');
 
+
+
 // on load
 // fetch all dogs
 // render and append all dog cards to the container
